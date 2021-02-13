0be30d5f444c92e7ff85e3c01c56fec2138bba7e
Jacky Peralta <38457193+jckyprlt@users.noreply.github.com>
2021-02-13 11:49:52 -0800

    forgot to comment out a user test

--- routes/user.js
+++ routes/user.js
@@ -4,20 +4,20 @@ import Users from '../models/user.js'
 var router = express.Router();
 
 // UNCOMMENT TO TEST
-const userInput = {
-    firstName: "j",
-    lastName: "p",
-    email: "jpp@gmail.com",
-    username: "jpppppppp",
-    password: "1234567"
-}
+// const userInput = {
+//     firstName: "j",
+//     lastName: "p",
+//     email: "jpp@gmail.com",
+//     username: "jpppppppp",
+//     password: "1234567"
+// }
 
-const user = new Users(userInput);
-user.save((err, document)=>{
-    if(err)
-        console.log(err);
-    console.log(document);
-});
+// const user = new Users(userInput);
+// user.save((err, document)=>{
+//     if(err)
+//         console.log(err);
+//     console.log(document);
+// });
 
 router.post("/snapGo/users", (req, res) => {
     const dbUser = req.body;
