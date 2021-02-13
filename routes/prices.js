e77ae98aff5ff89e5138bb192d58750d7c86f8fb
Jacky Peralta <38457193+jckyprlt@users.noreply.github.com>
2021-02-13 11:26:51 -0800

    added routes folder

---
+++ routes/prices.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import Prices from '../models/prices.js'
+
+var router = express.Router();
+
+// UNCOMMENT TO TEST
+/* const priceInput = {
+    item: "scissors",
+    stores: [
+        {
+            name: "hobby lobby",
+            rating: "5",
+            url: "hobbylobby.com",
+            address: {
+                zip: "93312",
+                street: "1234 fake"
+            },
+            prices: [
+                {
+                    date: "9/25/2020",
+                    amounts: "11.99"
+                },
+                {
+                    date: "9/26/2020",
+                    amounts: "12.50"
+                }
+            ]
+        },
+        {
+            name: "Target",
+            rating: "4",
+            url: "target.com",
+            address: {
+                zip: "93307",
+                street: "1234 not real"
+            },
+            prices: [
+                {
+                    date: "9/15/2020",
+                    amounts: "13.99"
+                },
+                {
+                    date:"9/16/2020",
+                    amounts: "10.50"
+
+                }
+            ]
+        }
+    ]
+}
+
+const p = new Prices(priceInput);
+p.save((err, document)=>{
+    if(err)
+        console.log(err);
+    console.log(document);
+}); */
+
+router.post("/snapGo/prices", (req, res) => {
+    const dbPrices = req.body;
+    Prices.create(dbPrices, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else{
+            res.status(201).send(data);
+        }
+    });
+});
+
+router.get("/snapGo/prices", (req, res) => {
+    Prices.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else{
+            res.status(200).send(data);
+        }
+    });
+});
+
+export default router;
---
+++ routes/user.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import Users from '../models/user.js'
+
+var router = express.Router();
+
+// UNCOMMENT TO TEST
+const userInput = {
+    firstName: "j",
+    lastName: "p",
+    email: "jpp@gmail.com",
+    username: "jpppppppp",
+    password: "1234567"
+}
+
+const user = new Users(userInput);
+user.save((err, document)=>{
+    if(err)
+        console.log(err);
+    console.log(document);
+});
+
+router.post("/snapGo/users", (req, res) => {
+    const dbUser = req.body;
+    Users.create(dbUser, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else{
+            res.status(201).send(data);
+        }
+    });
+});
+
+router.get("/snapGo/users", (req, res) => {
+    Users.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else{
+            res.status(200).send(data);
+        }
+    });
+});
+
+export default router;
