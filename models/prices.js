34550088644e13dbaf872591174fd72257b7de36
Jacky Peralta <38457193+jckyprlt@users.noreply.github.com>
2021-03-10 12:15:53 -0800

    reference user by the ObjectId

--- models/prices.js
+++ models/prices.js
@@ -1,19 +1,24 @@
 import mongoose from 'mongoose';
 
 const storeSchema = mongoose.Schema({
-    name: String,
+    strName: String,
     rating: Number,
     url: String,
     address:{
         zip: String,
         street: String
     },
-    prices:[{ date: Date, amounts: Number }] // Fix Date
+    prices:[{ date: {type: Date, default: Date.now}, amounts: Number }]
 });
 
 const priceSchema = mongoose.Schema({
     item: String,
     stores: [storeSchema],
+    userID:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'user',
+        required: true
+    }
 });
 
 export default mongoose.model('prices', priceSchema);
