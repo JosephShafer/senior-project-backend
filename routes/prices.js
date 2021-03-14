7a0a907391cb5c0cab93c36f77d71603db7da7d1
Jacky Peralta <38457193+jckyprlt@users.noreply.github.com>
2021-03-14 15:24:50 -0700

    removed testing comments

--- routes/prices.js
+++ routes/prices.js
@@ -1,60 +1,7 @@
 import express from 'express';
 import Prices from '../models/prices.js'
 
-var router = express.Router();
-
-// UNCOMMENT TO TEST
-/* const priceInput = {
-    item: "scissors",
-    stores: [
-        {
-            name: "hobby lobby",
-            rating: "5",
-            url: "hobbylobby.com",
-            address: {
-                zip: "93312",
-                street: "1234 fake"
-            },
-            prices: [
-                {
-                    date: "9/25/2020",
-                    amounts: "11.99"
-                },
-                {
-                    date: "9/26/2020",
-                    amounts: "12.50"
-                }
-            ]
-        },
-        {
-            name: "Target",
-            rating: "4",
-            url: "target.com",
-            address: {
-                zip: "93307",
-                street: "1234 not real"
-            },
-            prices: [
-                {
-                    date: "9/15/2020",
-                    amounts: "13.99"
-                },
-                {
-                    date:"9/16/2020",
-                    amounts: "10.50"
-
-                }
-            ]
-        }
-    ]
-}
-
-const p = new Prices(priceInput);
-p.save((err, document)=>{
-    if(err)
-        console.log(err);
-    console.log(document);
-}); */
+const router = express.Router();
 
 router.post("/", (req, res) => {
     const dbPrices = req.body;
--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,7 @@
 import express from 'express';
-import Users from '../models/user.js'
+import Users from '../models/user.js';
 
-var router = express.Router();
-
-// UNCOMMENT TO TEST
-/* const userInput = {
-    firstName: "j",
-    lastName: "p",
-    email: "jpp@gmail.com",
-    username: "jpppppppp",
-    password: "1234567"
-}
-
-const user = new Users(userInput);
-user.save((err, document)=>{
-    if(err)
-        console.log(err);
-    console.log(document);
-}); */
+const router = express.Router();
 
 router.post("/", (req, res) => {
     const dbUser = req.body;
