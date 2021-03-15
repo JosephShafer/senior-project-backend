701d56364722e8f9d7e9eeb5e35881d9a9b3ce98
Juan Villasenor <twinksy1@gmail.com>
2021-03-15 13:39:24 -0700

    Merged, confronted with auth0 error

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ import priceRouter from './routes/prices.js';
 import usersRouter from './routes/user.js';
 import forgotPassRouter from './routes/forgotPass.js';
 import resetPassRouter from './routes/resetPass.js';
+let WC = require("./Web-Crawler/WebCrawler.js");
+let sites = ["https://www.kaplanco.com/shop/arts-and-crafts/collage-and-craft-materials",
+	         "https://www.pinterest.com/caytonmuseum/arts-craft-ideas/",
+]
 
 const port = process.env.PORT || 8001;
 const app = express();
@@ -32,6 +36,23 @@ mongoose.connect(process.env.LOCAL_MONGO, {
 })
 .then(() => console.log('database connected...'))
 .catch(err => console.log(err));
+
+
+// JV. 03-15-2021: Manually merged branches
+app.post("/webcrawl", async function(req, res) {
+	let target = req.body.searchTerm;
+	console.log(`Received JSON response. Searching for ${target}`);
+	// All results will be written to files
+	for(let idx = 0; idx < sites.length; idx++) {
+		try {
+			await WC.crawl(idx, sites[idx], target);
+		} catch(err) {
+			console.log(err);
+		}
+	}
+	console.log("Finished web crawling");
+});
+
 
 /* Listener */
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
