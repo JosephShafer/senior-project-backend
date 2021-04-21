50e04efd547a5c03d13b4205ba2f58fdd5472411
Joseph Shafer <joeyshafer96@gmail.com>
2021-04-20 20:27:03 -0700

    forgot password backend work

--- routes/forgotPass.js
+++ routes/forgotPass.js
@@ -17,11 +17,16 @@ const transporter = nodemailer.createTransport({
 
 router.put('/', (req, res) => {
     const email = req.body.email;
+    const link = req.body.redirectURL;
+    console.log(email);
     Users.findOne({ email })
         .then(user => {
             if (user) {
                 const token = crypto.randomBytes(20).toString('hex');
-                return Users.findOneAndUpdate({ email }, 
+                return Users.findOneAndUpdate(
+                    { 
+                        email
+                    }, 
                     {
                         reset_password_token: token, 
                         reset_password_expires: Date.now() + 86400000  // Token expires in 24 hrs
@@ -33,9 +38,12 @@ router.put('/', (req, res) => {
             }    
         })
         .then(user => {
+            // console.log(user.reset_password_token)
             const token = user.reset_password_token;
             // encodes characters such as ?,=,/,&,:
-            const uri = encodeURIComponent(`://192.168.1.77:19000/--/reset_password/${token}`);
+            // hardcoded
+            // const uri = encodeURIComponent(`://192.168.1.18:19000/--/reset_password/${token}`);
+            console.log(`${link + '?ResetPasswordToken=' + token}`);
             const mailOptions = {
                 from: process.env.MAILER_EMAIL,
                 to: email,
@@ -46,7 +54,7 @@ router.put('/', (req, res) => {
                     <h3>Dear ${user.username},</h3>
                     <p>You've requested to reset your password, click the link below to do so.</p>
                     <br>
-                    <p>https://expo.io/--/to-exp/exp${uri}</p>
+                    <p>${link + '?ResetPasswordToken=' + token}</p>
                     <br>
                     <p>If you did not request this, ignore this email and your password will not change.</p>
                     <p>Thanks!</p>
