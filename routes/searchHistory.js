e63846a52e443433b20976b012063ab5628a4c6e
Joseph Shafer <joeyshafer96@gmail.com>
2021-04-21 21:27:56 -0700

    added some error handling

--- routes/searchHistory.js
+++ routes/searchHistory.js
@@ -5,13 +5,17 @@ const router = express.Router();
 
 router.post("/", (req, res) => {
     // console.log("GOT THIS FAR");
+    console.log(req.body)
     const dbSearch = req.body;
     Searches
         .create(dbSearch)
         .then(result => 
             res.status(200).json({success: true, result})
         )
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err)
+            res.status(404).end();
+        });
 });
 
 router.get("/", (req, res) => {
@@ -19,6 +23,7 @@ router.get("/", (req, res) => {
     Searches.find((err, data) => {
         if (err) {
             console.log(err);
+            res.status(404).end();
         } else{
             res.status(200).send(data);
         }
@@ -32,6 +37,10 @@ router.post("/getUsersResults", (req, res) => {
     .then(user => {
         console.log(user.searchTerms);
         res.json({usersSearches: user.searchTerms});
+    })
+    .catch(e => {
+        console.log("User has no searches")
+        res.status(404).end();
     })
 });
 
@@ -48,7 +57,10 @@ router.put("/", (req, res) => {
     .then(result => 
         res.status(200).json({success: true, result})
     )
-    .catch(err => console.log(err));
+    .catch(err => {
+        console.log(err)
+        res.status(404).end();
+    });
 });
 
 module.exports = router;
